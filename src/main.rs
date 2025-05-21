a2fcd975a83a81b7198307e61d5165841abc1908
Ajinkya Kokandakar <ajinkyakokandakar@gmail.com>
2025-05-21 00:34:24 -0500

    converted all expects to errors

--- src/main.rs
+++ src/main.rs
@@ -30,60 +30,75 @@ const EXTS: [&str; 20] = [
     "synctex(busy)",
 ];
 
-fn is_tex_aux<T: AsRef<Path>>(path: &T) -> bool {
+fn is_tex_aux<T: AsRef<Path>>(path: &T) -> Result<bool, std::io::Error> {
     // println!("checking path {}", path.as_ref().display());
     let pathref = path.as_ref();
     let ext = match pathref.extension() {
         Some(x) => x,
-        None => return false,
+        None => return Ok(false),
     }
     .to_str()
-    .expect(&format!(
-        "BUG: Problem OsStr to str for path {}",
-        pathref.display()
-    ));
+    .ok_or_else(|| {
+        Error::new(
+            std::io::ErrorKind::InvalidData,
+            format!("BUG: Problem OsStr to str for path {}", pathref.display()),
+        )
+    })?;
+    // .expect(&format!(
+    //     "BUG: Problem OsStr to str for path {}",
+    //     pathref.display()
+    // ));
 
-    EXTS.contains(&ext)
+    let fl = EXTS.contains(&ext)
         || pathref
             .to_str()
-            .expect(&format!(
-                "BUG: Problem OsStr to str for path {:}",
-                pathref.display()
-            ))
-            .contains(r#"synctex.gz"#) //TODO: remove unwrap
+            .ok_or_else(|| {
+                Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    format!("BUG: Problem OsStr to str for path {:}", pathref.display()),
+                )
+            })?
+            .contains(r#"synctex.gz"#); //TODO: remove unwrap
+    Ok(fl)
 }
 
-fn process_dir<T: AsRef<Path>>(fld: T) -> Vec<PathBuf> {
+fn process_dir<T: AsRef<Path>>(fld: T) -> Result<Vec<PathBuf>, std::io::Error> {
     println!("Processing directory {}", fld.as_ref().display());
-    let files = read_dir(&fld)
-        .expect(&format!(
-            "error reading directory for {}",
-            fld.as_ref().display()
-        ))
+    let files = read_dir(&fld)?
         .filter_map(|res| res.ok())
         .map(|entry| entry.path())
-        .filter(|path| path.is_file() && is_tex_aux(&path))
-        .collect::<Vec<_>>();
+        .filter(|path| {
+            path.is_file()
+                && is_tex_aux(&path).unwrap_or_else(|e| {
+                    eprintln!("{}, Skipping the file {}", e, path.display());
+                    false
+                })
+        }) // https://users.rust-lang.org/t/collections-filter-and-error-handling/32871/2
+        .collect::<Vec<PathBuf>>();
     // println!("{:?}", files);
-    files
+    Ok(files)
 }
 
-fn process_file<T: AsRef<Path>>(path: T) -> Vec<PathBuf> {
+fn process_file<T: AsRef<Path>>(path: T) -> Result<Vec<PathBuf>, std::io::Error> {
     let fld = path.as_ref();
     let pardir = match fld.extension() {
         Some(x) => match x.to_str() {
-            Some("tex") | Some("pdf") => fld.parent().expect(&format!(
-                "Cannot get parent directory for {}",
-                fld.display()
+            Some("tex") | Some("pdf") => fld.parent().ok_or_else(|| {
+                Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    format!("Cannot get parent directory for {}", fld.display()),
+                )
+            }),
+            _ => Err(Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "input must a directory, tex, or pdf file",
             )),
-            _ => {
-                panic!("input must a directory, tex, or pdf file");
-            }
         },
-        _ => {
-            panic!("input must a directory, tex, or pdf file");
-        }
-    };
+        None => Err(Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "input must a directory, tex, or pdf file",
+        )),
+    }?;
 
     let pardir = match pardir.as_os_str().is_empty() {
         true => Path::new("."),
@@ -92,25 +107,50 @@ fn process_file<T: AsRef<Path>>(path: T) -> Vec<PathBuf> {
     // println!("Parent directory is {}", pardir.display());
     let fname = fld
         .file_stem()
-        .expect("something wrong with file stem")
+        .ok_or_else(|| {
+            Error::new(
+                std::io::ErrorKind::InvalidFilename,
+                format!(
+                    "something wrong with file stem for the filename {}",
+                    fld.display(),
+                ),
+            )
+        })?
         .to_str()
-        .expect("couldnt convert to str");
+        .ok_or_else(|| {
+            Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Something went wrong when priocessing {}", fld.display()),
+            )
+        })?;
     // println!("{:?}", fname);
-    let files = read_dir(pardir)
-        .expect("error reading directory, {pardir:?}")
+    let files = read_dir(pardir)?
         .filter_map(|res| res.ok())
         .map(|entry| entry.path())
-        .filter(|path| {
-            path.file_name()
-                .expect("something wrng with filename")
-                .to_str()
-                .expect("msg")
-                .starts_with(fname)
-                && is_tex_aux(&path)
+        .filter(|path| match path.file_name() {
+            Some(x) => match x.to_str() {
+                Some(x) => {
+                    x.starts_with(fname)
+                        && is_tex_aux(&path).unwrap_or_else(|e| {
+                            eprintln!("{}, Skipping the file {}", e, path.display());
+                            false
+                        })
+                }
+                None => {
+                    eprintln!(
+                        "Something off when proessing file {}, skipping",
+                        path.display()
+                    );
+                    false
+                }
+            },
+            None => {
+                eprintln!("couldn't process file {}, skipping", path.display());
+                false
+            }
         })
         .collect::<Vec<_>>();
-    // println!("{:?}", files);
-    files
+    Ok(files)
 }
 
 fn process_path(fld: PathBuf) -> Result<Vec<PathBuf>, std::io::Error> {
@@ -123,8 +163,8 @@ fn process_path(fld: PathBuf) -> Result<Vec<PathBuf>, std::io::Error> {
         Err(e) => Err(e),
     };
     match isdir? {
-        true => Ok(process_dir(fld)),
-        false => Ok(process_file(fld)),
+        true => process_dir(fld),
+        false => process_file(fld),
     }
 }
 
